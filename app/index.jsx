c49a2b3aa833acc2a7f9ae21f1aed7742ea19f47
RenanKr4t0s <redrums88@gmail.com>
2024-11-26 17:39:46 -0300

    Estruturas e Layout creation

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aora is uploading!</Text>
+      <Link href="/profile" style={{color:"blue"}}>Vá para o Perfil</Link>
       <StatusBar style="auto" />
     </View>
   );
---
+++ readme.md
@@ -0,0 +1,4 @@
+# App jsMastery
+Criaremos o app em React Native jsMastery para executar diversas funcionalidades mobile e trabalhar com outras bibliotecas.
+
+No momento fazendo rotas, utilizando Link e expo router para transitar entre telas.
